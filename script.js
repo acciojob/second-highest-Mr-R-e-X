5c4797ddb1a143463fa9aab7c933b0930da265c8
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-20 15:58:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,18 @@
-//your JS code here. If required.
 function secondHighest() {
-	 const input = prompt("Enter comma-separated integers:");
-	const arr = input.split(',').map(Number);
-	if (arr.length <= 1) {
-        return undefined;
+    const input = prompt("Enter comma-separated integers:");
+    const arr = input.split(',').map(Number);
+
+    if (arr.length <= 1 || [...new Set(arr)].length === 1) {
+        return -Infinity;
     }
     
-    const sortedArr =  arr.sort((a, b) => b - a);
+    const sortedArr = arr.sort((a, b) => b - a);
     
-        if (sortedArr.length === 1) {
-        return undefined;
-    } 
-	for(let i = 1; i < sortedArr.length; i++) {
-    if(sortedArr[i] !== sortedArr[0]) {
-        return sortedArr[i];
+    for(let i = 1; i < sortedArr.length; i++) {
+        if(sortedArr[i] !== sortedArr[0]) {
+            return sortedArr[i];
+        }
     }
-}
-	
-    return arr[1]; 
 }
 const result = secondHighest();
-alert(result); 
+alert(result);  
