c3ec2152f9eccfe353b45d7a69241c4638dfc87a
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-04-20 15:43:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 //your JS code here. If required.
+function secondHighest(arr) {
+	if (arr.length <= 1) {
+        return -Infinity;
+    }
+    
+    arr.sort((a, b) => b - a);
+    
+    if (arr[0] === arr[arr.length - 1]) {
+        return -Infinity;
+    } 
+    return arr[1];
+}
