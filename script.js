546072c489b58d4087843751fd610e51dd4447a7
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-05-02 23:35:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-function secondHighest() {
-    if (arr.length <= 1 || new Set(arr).size === 1) {
-        return -Infinity;
-    }
-    
-    const sortedArr = [...new Set(arr)].sort((a, b) => b - a);
-    
-    return sortedArr[1];
-}
-const result = secondHighest();
-alert(result);  
+\
