2647defb8f8d8cef9915840789afaa447fdb536c
Souvik Hazra <117592326+Mr-R-e-X@users.noreply.github.com>
2024-05-02 23:43:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-\
+function secondHighest(arr) {
+			 if (arr.length < 2) {
+        return -Infinity;
+    }
+    const uniqueArr = [];
+    arr.forEach(element => {
+        if (!uniqueArr.includes(element)) {
+            uniqueArr.push(element);
+        }
+    });
+    if (uniqueArr.length === 1) {
+        return -Infinity;
+    }
+    uniqueArr.sort((a, b) => b - a);
+    return uniqueArr[1];
+		}
+
+		function Main() {
+			var n = prompt("Enter the number of elements");
+			var arr = [];
+			for (var i = 0; i < n; i++) {
+				arr[i] = prompt("Enter element " + (i+1));
+			}
+			alert(secondHighest(arr));
+		}
+		Main();
